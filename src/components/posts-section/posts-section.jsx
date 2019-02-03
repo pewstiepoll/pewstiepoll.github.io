ec24846102944114d73df3545a299afaa3125d9b
Vadym <wadedevelop@gmail.com>
2019-02-03 16:31:17 +0200

    <PostSection />: Finished Mobile-first adaptive markup.

--- src/components/posts-section/posts-section.jsx
+++ src/components/posts-section/posts-section.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import styles from './posts-section.module.css';
-import detailsStyle from './posts-details.module.css';
+import postStyles from './posts-details.module.css';
 
 import buttonStyles from './button.module.css';
 
@@ -36,35 +36,45 @@ Button.propTypes = {
   link: PropTypes.string,
 };
 
+const Post = () => (
+  <article className={postStyles.container}>
+    <img
+      className={postStyles.image}
+      src="https://contentberg.theme-sphere.com/wp-content/uploads/2018/09/shutterstock_555069928-370x370.jpg"
+      alt="acticle_img"
+    />
+    <div className={postStyles.details}>
+      <div className={postStyles.header}>
+        <span className={postStyles.date}>MAY 15 2018</span>
+        <span className={postStyles.categoryLabel}>Gadgets</span>
+      </div>
+      <div className={postStyles.main}>
+        <h2 className={postStyles.title}>
+          Explore the Sea world with Deep Sea Fishing
+        </h2>
+        <span className={postStyles.description}>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima,
+          porro.
+        </span>
+      </div>
+      <div className={postStyles.footer}>
+        <Button type="primary">Read More</Button>
+      </div>
+    </div>
+  </article>
+);
+
+// TODO: Add pagination funcionality later
 export default function PostSection() {
-  // TODO: Mobile Adaptation
   return (
     <div className={styles.wrapper}>
-      <article className={styles.container}>
-        <img
-          className={styles.image}
-          src="https://contentberg.theme-sphere.com/wp-content/uploads/2018/09/shutterstock_555069928-370x370.jpg"
-          alt="acticle_img"
-        />
-        <div className={detailsStyle.container}>
-          <div className={detailsStyle.header}>
-            <span className={detailsStyle.date}>MAY 15 2018</span>
-            <span className={detailsStyle.categoryLabel}>Gadgets</span>
-          </div>
-          <div className={detailsStyle.main}>
-            <h2 className={detailsStyle.title}>
-              Explore the Sea world with Deep Sea Fishing
-            </h2>
-            <span className={detailsStyle.description}>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima,
-              porro.
-            </span>
-          </div>
-          <div className={detailsStyle.footer}>
-            <Button type="primary">Read More</Button>
-          </div>
-        </div>
-      </article>
+      <h1 className={styles.title}>Latest</h1>
+      <div className={styles.posts}>
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+      </div>
     </div>
   );
 }
